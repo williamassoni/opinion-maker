22d239672cc81b13129fc6524ad89a4746bd2c57
assoni <will.assoni@gmail.com>
2017-11-19 17:08:14 -0200

    first commit

---
+++ src/main/java/com/opinionmaker/opinionmaker/OpinionmakerApplication.java
@@ -0,0 +1,12 @@
+package com.opinionmaker.opinionmaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpinionmakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpinionmakerApplication.class, args);
+	}
+}
